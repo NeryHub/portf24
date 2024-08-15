918776a0aef0a30a37cb33beb3ea52e95d200912
NeryHub <125278013+NeryHub@users.noreply.github.com>
2024-08-15 09:08:29 +0000

    adicionei versao 2 na web

--- prtweb24/src/App.js
+++ prtweb24/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nery Nery Nery Nery
+          Nery Nery Nery Nery versao 2
          </p>
         <a
           className="App-link"
